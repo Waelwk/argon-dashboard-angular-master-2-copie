b3e00280f9f1f98546735742398ae4030fca2be9
Waelwk <wael-kamel@outlook.fr>
2025-04-29 23:16:36 +0100

    chore: update dependencies and improve test setup
    - Updated Angular core and CLI dependencies to version 15.2.10.
    - Upgraded Bootstrap from version 4.6.1 to 5.3.5.
    - Changed codelyzer dependency from version 6.0.2 to ^0.0.28.
    - Replaced Protractor with karma-webpack version ^5.0.1.
    - Modified test.ts to enhance Angular testing environment setup by adding teardown options and removing unnecessary code.

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,3 @@
-// This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
@@ -7,15 +5,12 @@ import {
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
-declare const require: any;
-
-// First, initialize the Angular testing environment.
+// First initialize the Angular testing environment
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
+  
+  platformBrowserDynamicTesting(),
+  { 
+    teardown: { destroyAfterEach: false } // Add this line
+  }
 );
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-context.keys().forEach(context);
-// And load the modules.
-context.keys().map(context);
